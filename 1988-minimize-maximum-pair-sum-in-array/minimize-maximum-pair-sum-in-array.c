947fe84925fbb197596ea5622331135581c53e78
praneeth-varma-17 <praneethvarma2728@gmail.com>
2026-08-05 20:05:16 +0530

    Time: 271 ms (6.70%) | Memory: 16.2 MB (100.00%) - LeetSync

---
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.c
@@ -0,0 +1,78 @@
+
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+
+int minPairSum(int* nums, int numsSize){
+
+    mergeSort(nums, 0, numsSize - 1);
+
+    int max =INT_MIN;
+
+    for(int i = 0 ; i < numsSize; i+=2){
+        int sum = nums[i] + nums[numsSize - 1 -i];
+
+        if(sum > max){
+            max = sum;
+        }
+    }
+
+    return max;
+
+}
