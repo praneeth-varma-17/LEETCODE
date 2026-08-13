7cd6ea0e11382549085e645fefdee17d060ac65b
praneeth-varma-17 <praneethvarma2728@gmail.com>
2026-08-13 16:57:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (90.43%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.c
@@ -0,0 +1,17 @@
+bool isThree(int n) {
+
+    int count = 0;
+
+    for(int i = 1; i <= n/2; i++){
+        if(n % i == 0){
+            count++;
+        }
+    }
+
+    if(count == 2){
+        return true;
+    }
+
+    return false;
+    
+}
