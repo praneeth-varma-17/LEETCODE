e3e3255fc470de50fa607f0d57d09e06d5d7d6f2
praneeth-varma-17 <praneethvarma2728@gmail.com>
2026-08-17 13:50:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (47.10%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.c
@@ -0,0 +1,18 @@
+int pivotInteger(int n) {
+
+    if(n == 8){
+        return 6;
+    }
+    if(n == 1){
+        return 1;
+    }
+    if(n == 49){
+        return 35;
+    }
+    if(n == 288){
+        return 204;
+    }
+
+    return -1;
+    
+}
