dc25a77f8941406bddfe261c93cf7e7b63111507
praneeth-varma-17 <praneethvarma2728@gmail.com>
2026-08-23 10:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (43.28%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.c
@@ -0,0 +1,29 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+
+    int max = INT_MIN;
+
+    for(int i = 0; i < candiesSize; i++){
+        if(candies[i] > max){
+            max = candies[i];
+        }
+    }
+
+    int size = candiesSize; 
+    
+
+    bool* result = malloc(size * sizeof(bool));
+
+    for (int i = 0; i < candiesSize; i++) {
+        result[i] = (candies[i] + extraCandies >= max );
+    }
+    
+
+    *returnSize = candiesSize; 
+    
+
+    return result;
+}
+    
